d3911e5b9117a894e96c97504791dfecf2119568
Brien Thomas <thomasbrien94@gmail.com>
2022-06-07 14:57:37 -0700

    imported the animal images and finished coding the Main component which will loop through the animals array

--- src/AnimalRender.js
+++ src/AnimalRender.js
@@ -3,8 +3,9 @@ import './Animal/Animal.css';
 
 export default function AnimalRender({ name, type, says, top, left }) {
   return (
-    <div className='.animal' style={{ top, left }}>
-      <h2>{name}</h2>
+    <div className='animal' style={{ top, left }}>
+      <img src={`./Animal/{name}.svg`} />
+      <h2 className='animal name'>{name}</h2>
       <p>{type}</p>
       <span>{says}</span>
     </div>
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import AnimalRender from '../AnimalRender';
 import './Main.css';
+import backgroundImg from '../background.png';
 
-export default function Main() {
-  return <main>
-    {
-      animals.map(animal, i) => 
-    }
-  </main>;
-}
+export default function Main({ animals }) {
+  return (
+    <main style={{ backgroundImage: `url(${backgroundImg})`}}>
+      {
+        animals.map((animal, i) => <AnimalRender
+          key={animal.name + i}
+          {...animal} />)
+      }
+    </main>
+  );}
